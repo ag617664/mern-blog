2ec41ec7eb9ea4d7ea2e126042e642edb4d58738
ag617664 <95614809+ag617664@users.noreply.github.com>
2024-02-03 15:24:21 +0530

    signup page

--- api/Controllers/auth.controller.js
+++ api/Controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from '../Models/user.model.js'
 import bcryptjs from 'bcryptjs';
 import { nextTick } from 'process';
+import {errorHandler} from '../utils/error.js'
 
 export const signup=async(req,res,next)=>{
-    const {user,email,password}=req.body;
+    const {username,email,password}=req.body;
 
     if(!username || !email || !password || username==='' || email ==='' || password===''){
         next(errorHandler(400,'All fields are required'));
