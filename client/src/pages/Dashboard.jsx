69df76ecd35a448d926fe4a5a9867959f74389a0
ag617664 <95614809+ag617664@users.noreply.github.com>
2024-02-07 22:10:31 +0530

    sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom'
+import { useState } from 'react';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 function Dashboard() {
+  const location=useLocation();
+  const [tab,setTab]=useState('');
+  useEffect(()=>{
+    const urlParams=new URLSearchParams(location.search);
+    const tabFromUrl=urlParams.get('tab');
+    setTab(tabFromUrl);
+  },[location.search]);
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className="md:w-56">
+        <DashSidebar/>
+
+      </div>
+      {tab==='profile' && <DashProfile/>}
+    </div>
   )
 }
 
