a3b5bcf33e517ef8cb201eb340df826cba41348d
ag617664 <95614809+ag617664@users.noreply.github.com>
2024-02-09 16:58:39 +0530

    image uploading

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,16 +1,90 @@
-import { Button, TextInput } from 'flowbite-react';
-import React from 'react'
-import { useSelector } from 'react-redux'
+import { Alert, Button, Modal, ModalBody, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { useDispatch } from 'react-redux';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-    const {currentUser}=useSelector(state=>state.user);
+    const { currentUser, error, loading } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({});
+  const filePickerRef = useRef();
+//   const dispatch = useDispatch();
+    const handleImageChange=(e)=>{
+        const file=e.target.files[0];
+        console.log(file);
+        if(file){
+
+            setImageFile(file);
+            setImageFileUrl(URL.createObjectURL(file));
+            console.log(imageFile);
+            console.log(imageFile,imageFileUrl);
+        }
+    }
+    useEffect(()=>{
+        if(imageFile){
+            uploadImage();
+        }
+    },[imageFile])
+
+    const uploadImage=async()=>{
+        const storage=getStorage(app);
+        const fileName=new Date().getTime() + imageFile.name
+        const storageRef=ref(storage,fileName);
+        const uploadTask=uploadBytesResumable(storageRef,imageFile);
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+              const progress =
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      
+              setImageFileUploadProgress(progress.toFixed(0));
+            },
+            (error) => {
+              setImageFileUploadError(
+                'Could not upload image (File must be less than 2MB)'
+              );
+              setImageFileUploadProgress(null);
+              setImageFile(null);
+              setImageFileUrl(null);
+              setImageFileUploading(false);
+            },
+            () => {
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                setImageFileUrl(downloadURL);
+                setFormData({ ...formData, profilePicture: downloadURL });
+                setImageFileUploading(false);
+              });
+            }
+          );
+        };
+      
+    
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
         <form className='flex flex-col gap-4'>
-            <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+            <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} className='hidden'/>
+            <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={()=>filePickerRef.current.click()}>
 
-            <img src={currentUser.profilePicture} alt="user" className='rounded-full w-full h-full border-8 border-[lightgray] object-cover'/>
+            <img src={imageFileUrl || currentUser.profilePicture} alt="user" className='rounded-full w-full h-full border-8 border-[lightgray] object-cover'/>
             </div>
             <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
             <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -18,8 +18,8 @@ export default function DashSidebar() {
         <Sidebar.Items>
             <Sidebar.ItemGroup>
                 <Link to='/dashboard?tab=profile'>
-                <Sidebar.Item active={tab==='profile'} icon={HiUser} label={"User"} labelColor='dark'>Profile</Sidebar.Item>
-                <Sidebar.Item  icon={HiArrowSmRight} className="cursor-pointer">Sign Out</Sidebar.Item>
+                <Sidebar.Item active={tab==='profile'} icon={HiUser} label={"User"} labelColor='dark' as='div'>Profile</Sidebar.Item>
+                <Sidebar.Item  icon={HiArrowSmRight} className="cursor-pointer" as='div'>Sign Out</Sidebar.Item>
                 </Link>
             </Sidebar.ItemGroup>
         </Sidebar.Items>
